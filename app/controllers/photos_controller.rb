782f3cb6a93bfed45973c2295be4669525010623
Ryan Fisher <ryancfisher@gmail.com>
2015-05-26 21:34:35 -0700

    Fix photos show and render locals instead of using helper methods

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,16 +1,10 @@
 # Controller for the photos
 class PhotosController < ApplicationController
   def index
+    render locals: { photos_json: Photo.limit(40).to_json }
   end
 
   def show
+    render locals: { photo_json: Photo.find(params.fetch(:id)).to_json }
   end
-
-  private
-
-  def photos_json
-    Photo.limit(40).to_json
-  end
-
-  helper_method :photos_json
 end
