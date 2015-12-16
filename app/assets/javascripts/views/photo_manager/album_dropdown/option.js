bf1691dc4dbe9eeea8cd0a4010ffb01e27cda29a
Ryan Fisher <ryancfisher@gmail.com>
2015-12-16 00:43:14 -0800

    Add albums to album feed

--- app/assets/javascripts/views/photo_manager/album_dropdown/option.js
+++ app/assets/javascripts/views/photo_manager/album_dropdown/option.js
@@ -2,12 +2,19 @@ namespace('Fotio.Views.PhotoManager.AlbumDropdown', function(exports) {
   exports.Option = Backbone.View.extend({
     className: 'option album',
 
+    events: {
+      'click': 'addSelectedPhotos'
+    },
+
     initialize: function() {
       this.render();
     },
 
     render: function() {
       this.$el.text(this.model.get('title'));
+    },
+
+    addSelectedPhotos: function() {
     }
   })
 });
--- app/assets/javascripts/views/photo_manager/album_feed.js
+++ app/assets/javascripts/views/photo_manager/album_feed.js
@@ -2,6 +2,26 @@
 
 namespace('Fotio.Views.PhotoManager', function(exports) {
   exports.AlbumFeed = exports.BaseView.extend({
-    el: '#album-manager-feed'
+    el: '#album-manager-feed',
+
+    initialize: function() {
+      this.collection = new Fotio.Collections.User.Albums()
+      this.listenTo(this.collection, 'sync', this.render)
+      this.collection.fetch();
+    },
+
+    render: function() {
+      this.collection.each(_.bind(this.appendAlbum, this))
+    },
+
+    appendAlbum: function(model) {
+      var album = $('<div>', { text: model.get('title'), class: 'album-cover' });
+      album.append($('<i>', { class: 'fa fa-folder' }));
+      this.$('.feed').append(album)
+    },
+
+    cleanUp: function() {
+      this.$('.album-cover').remove()
+    }
   });
 });
