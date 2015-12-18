26ca7030ea57ffbfd9eea348a4252343119600fc
Ryan Fisher <ryancfisher@gmail.com>
2015-12-17 22:52:39 -0800

    Render feed on album editor page

--- app/assets/javascripts/views/photo_manager/album_editor.js
+++ app/assets/javascripts/views/photo_manager/album_editor.js
@@ -2,6 +2,14 @@
 
 namespace('Fotio.Views.PhotoManager', function(exports) {
   exports.AlbumEditor = exports.BaseView.extend({
-    el: '#album-editor'
+    el: '#album-editor',
+
+    initialize: function() {
+      this.feed = new exports.PhotoFeed({
+        el: this.$('.feed'),
+        collection: this.model.get('photos')
+      });
+      this.feed.render();
+    },
   });
 });
