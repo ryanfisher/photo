7a510a6b4f71684f8941be37f3eb23aab0fc925e
Ryan Fisher <ryancfisher@gmail.com>
2015-12-26 17:40:16 -0800

    Add notices for ssaves when adding photos to albums

--- app/assets/javascripts/views/photo_manager/album_dropdown/option.js
+++ app/assets/javascripts/views/photo_manager/album_dropdown/option.js
@@ -16,12 +16,25 @@ namespace('Fotio.Views.PhotoManager.AlbumDropdown', function(exports) {
 
     addSelectedPhotos: function() {
       var photos = this.feed.selected_photos();
-      if (photos.length === 0) return this.showErrorNotice();
+      if (photos.length === 0) return this.showNoPhotosNotice();
       _.each(photos, this.model.addPhoto, this.model)
-      this.model.save()
+      this.model.save({}, {
+        success: this.showSuccessNotice,
+        error: this.showErrorNotice
+      });
+    },
+
+    showSuccessNotice: function(model) {
+      var message = 'Selected photos successfully added to album (' + model.get('title') + ').';
+      Backbone.trigger('notice', { message: message });
     },
 
     showErrorNotice: function() {
+      var message = 'There was an error adding the photos to the album.';
+      Backbone.trigger('notice', { message: message, status: 'danger' });
+    },
+
+    showNoPhotosNotice: function() {
       var message = 'Must select photos to add to album.';
       Backbone.trigger('notice', { message: message, status: 'danger' });
     }
