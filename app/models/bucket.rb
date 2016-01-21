83f119fcbf6075c8b98a96c117ce42ab3f79750c
Ryan Fisher <ryancfisher@gmail.com>
2016-01-20 21:21:05 -0800

    Set up bucket connection

--- app/models/bucket.rb
+++ app/models/bucket.rb
@@ -2,4 +2,29 @@ class Bucket < ActiveRecord::Base
   enum provider: [ :aws, :google, :rackspace ]
   attr_encrypted :access_key_id, key: Rails.application.secrets.access_key
   attr_encrypted :secret_access_key, key: Rails.application.secrets.secret_key
+
+  # Aliased for use with rackspace
+  alias_attribute :username, :access_key_id
+  alias_attribute :api_key, :secret_access_key
+
+  # A fog connection to the bucket
+  def connection
+    @_connection ||= begin
+      connection_keys = Hash[keys.zip([access_key_id, secret_access_key])]
+      Fog::Storage.new(connection_keys.merge(provider: provider))
+    end
+  end
+
+  private
+
+  def keys
+    case provider
+    when 'aws'
+      [:aws_access_key_id, :aws_secret_access_key]
+    when 'google'
+      [:google_storage_access_key_id, :google_storage_secret_access_key]
+    when 'rackspace'
+      [:rackspace_username, :rackspace_api_key]
+    end
+  end
 end
--- spec/models/bucket_spec.rb
+++ spec/models/bucket_spec.rb
@@ -2,4 +2,61 @@ require 'rails_helper'
 
 describe Bucket, type: :model do
   it { is_expected.to respond_to :name }
+
+  describe '#connection' do
+    let(:connection) { double(:connection) }
+
+    before do
+      subject.provider = provider
+      subject.access_key_id = 'access key'
+      subject.secret_access_key = 'secret key'
+      expect(Fog::Storage).to receive(:new)
+        .with(a_hash_including(config_hash)) { connection }
+    end
+
+    describe 'when provider is google' do
+      let(:provider) { :google }
+      let(:config_hash) do
+        {
+          provider: 'google',
+          google_storage_access_key_id: 'access key',
+          google_storage_secret_access_key: 'secret key'
+        }
+      end
+
+      it 'should initialize Fog::Storage with correct hash' do
+        expect(subject.connection).to be connection
+      end
+    end
+
+    describe 'when provider is aws' do
+      let(:provider) { :aws }
+      let(:config_hash) do
+        {
+          provider: 'aws',
+          aws_access_key_id: 'access key',
+          aws_secret_access_key: 'secret key'
+        }
+      end
+
+      it 'should initialize Fog::Storage with correct hash' do
+        expect(subject.connection).to be connection
+      end
+    end
+
+    describe 'when provider is google' do
+      let(:provider) { :rackspace }
+      let(:config_hash) do
+        {
+          provider: 'rackspace',
+          rackspace_username: 'access key',
+          rackspace_api_key: 'secret key'
+        }
+      end
+
+      it 'should initialize Fog::Storage with correct hash' do
+        expect(subject.connection).to be connection
+      end
+    end
+  end
 end
