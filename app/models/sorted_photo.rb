a38205d8976cfbdaf8a0aa725a921c447f1382a9
Ryan Fisher <ryancfisher@gmail.com>
2016-01-26 22:45:32 -0800

    Add uniqueness validation

--- app/models/sorted_photo.rb
+++ app/models/sorted_photo.rb
@@ -2,6 +2,8 @@ class SortedPhoto < ActiveRecord::Base
   belongs_to :album, touch: true
   belongs_to :photo
 
+  validates_uniqueness_of :photo_id, scope: :album_id
+
   def simple_json
     {
       photo_id: photo.id,
