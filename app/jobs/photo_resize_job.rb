a893dcbd894c433b3279da839ec02b2aac1fe499
Ryan Fisher <ryancfisher@gmail.com>
2016-02-25 23:09:40 -0800

    Fix photo resize job issue

--- app/jobs/photo_resize_job.rb
+++ app/jobs/photo_resize_job.rb
@@ -2,6 +2,6 @@ class PhotoResizeJob < ActiveJob::Base
   queue_as :default
 
   def perform(*args)
-    Photo.find(args[0]['photo_id']).create_sized_images
+    Photo.find(args[0][:photo_id]).create_sized_images
   end
 end
