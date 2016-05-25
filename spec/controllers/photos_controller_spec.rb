66158955a055fded3eb633a24915406370048931
Ryan Fisher <ryancfisher@gmail.com>
2016-05-24 23:25:18 -0700

    Fix photos show controller

--- spec/controllers/photos_controller_spec.rb
+++ spec/controllers/photos_controller_spec.rb
@@ -10,6 +10,23 @@ describe PhotosController, type: :controller do
   end
 
   describe 'GET show' do
+    let(:user) do
+      User.create!(
+        email: 'kylo@firstorder.com',
+        password: 'ihatehansolo',
+        password_confirmation: 'ihatehansolo'
+      )
+    end
+    let(:photo) do
+      Photo.create(
+        file_key: '123456',
+        url: '/photo',
+        user_id: user.id
+      )
+    end
+
+    before { get :show, id: photo.id }
+
     it 'responds with 200 OK' do
       expect(response.status).to be 200
     end
